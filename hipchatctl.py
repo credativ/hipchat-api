7f8955d0723a777fc48d0d1f7e6fe161736e410f
Michael Sprengel <michael.sprengel@credativ.de>
2017-01-10 11:20:44 +0100

    Adjust license

--- hipchatctl.py
+++ hipchatctl.py
@@ -5,8 +5,6 @@
 #
 # powered by Swisscom Health AG | www.swisscom.com/health
 #
-# Copyright (c) 2017 Michael Sprengel
-# 
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
 # "Software"), to deal in the Software without restriction, including
